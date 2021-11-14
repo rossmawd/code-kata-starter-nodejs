a84de006ac5d5e12da02dc0609735f6367ad9b1e
Ross Mawdsley <rossmawd@gmail.com>
2021-11-14 20:59:13 +0000

    successfully importing a class and running a test on its function

---
+++ code/jaden.js
@@ -0,0 +1,12 @@
+
+module.exports = class Jaden {
+  constructor() {
+    console.log('IT IMPORTED')
+  }
+
+  multiply(num) {
+    return num * 3
+  }
+}
+
+
--- spec/starter_spec.js
+++ spec/starter_spec.js
@@ -1,5 +1,17 @@
+const jaden = require('../code/jaden')
+
 describe('A test suite', () => {
+  let instance; 
+  beforeEach(() => {
+    instance = new jaden;
+  })
   it('should pass', () => {
     expect(true).toBe(true)
   })
+
+  it('should behave...', () => {
+    expect(instance.multiply(2)).toEqual(4)
+
+    // expect(jaden.multiply(2)).toEqual(4)
+  });
 })
